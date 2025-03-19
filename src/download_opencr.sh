dc36cfd1f6c831624ca4e0d8ad91161f46bfab5e
Simon <simonschwaiger1308@gmail.com>
2025-03-19 09:22:45 +0100

    Predownload opencr firmware if no internet available

---
+++ src/download_opencr.sh
@@ -0,0 +1,5 @@
+# Based on: https://emanual.robotis.com/docs/en/platform/turtlebot3/opencr_setup/#opencr-setup
+mkdir tmp && cd tmp
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+tar -xvf opencr_update.tar.bz2
+cd ..
